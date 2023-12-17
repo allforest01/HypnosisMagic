511139a13496a641ef62838b2e49e16a0605cf4c
allforest01 <allforest01@gmail.com>
2023-12-17 20:14:37 +0700

    Changed variable names.

--- src/common/data_manager.cpp
+++ src/common/data_manager.cpp
@@ -1,21 +1,21 @@
 #include "data_manager.h"
 
-void BufToPacketBox(std::vector<uchar> &buf, PacketBox &box, int id, char type, int datasize) {
-    datasize -= 7;
-    int num = (int) buf.size() / datasize + (((int) buf.size() % datasize) != 0);
+void BufToPacketBox(std::vector<uchar> &buf, PacketBox &box, int id, char type, int data_size) {
+    data_size -= 7;
+    int num = (int) buf.size() / data_size + (((int) buf.size() % data_size) != 0);
     box.data.clear();
     box.id = id;
     box.type = type;
     box.size = num;
     box.data.resize(num);
     for (short i = 0; i < num; i++) {
-        auto segBeg = buf.begin() + i * datasize;
-        auto segEnd = std::min(buf.begin() + (i + 1) * datasize, buf.end());
+        auto seg_beg = buf.begin() + i * data_size;
+        auto seg_end = std::min(buf.begin() + (i + 1) * data_size, buf.end());
         box.data[i].assign((char*)&id, (char*)&id + 2);
         box.data[i].push_back(type);
         box.data[i].insert(box.data[i].end(), (char*)&num, (char*)&num + 2);
         box.data[i].insert(box.data[i].end(), (char*)&i, (char*)&i + 2);
-        box.data[i].insert(box.data[i].end(), segBeg, segEnd);
+        box.data[i].insert(box.data[i].end(), seg_beg, seg_end);
     }
 }
 
